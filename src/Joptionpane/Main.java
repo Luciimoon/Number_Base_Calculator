71eeab529bc70919749d64e38c1c15ffb8ae5e04
compsci <mitchell_wallace@ymail.com>
2018-11-30 10:42:53 -0700

    commit.2

---
+++ src/Joptionpane/Main.java
@@ -0,0 +1,17 @@
+package Joptionpane;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+        public static void main(String... args){
+
+            gui Frame = new gui();
+            Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Frame.setLocationRelativeTo(null);
+            Frame.setSize(275,180);
+            Frame.setVisible(true);
+        }
+    }
+
+
