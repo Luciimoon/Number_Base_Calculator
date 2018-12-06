a7bbc059a8c889b68b301d6fba8b5537a9601119
mitch <mitchell_wallace@ymail.com>
2018-12-06 00:34:16 -0700

    Completed action listener

--- src/Joptionpane/gui.java
+++ src/Joptionpane/gui.java
@@ -7,23 +7,28 @@ import java.awt.event.ActionListener;
 
 public class gui extends JFrame implements ActionListener {
 
-    private JTextField First_Num, Second_Num, First_Base, Second_Base, end;
+    private JTextField First_Num, Second_Num, First_Base, Second_Base, End_Base, end;
+    private JLabel First_Num_Label, Second_Num_Label, First_Base_Label, Second_Base_Label, End_Base_Label;
     private JLabel text;
     private JButton Cal;
 
     public gui() {
-        super("gui");
+        super("Calculator");
 
         setLayout(new GridBagLayout());
         GridBagConstraints layout = new GridBagConstraints();
 
+        //do dis thing
+        First_Num_Label = new JLabel("First num");
+        Second_Num_Label = new JLabel("Second num");
+        First_Base_Label = new JLabel("First Base");
+        Second_Base_Label = new JLabel("Second Base");
+        End_Base_Label = new JLabel("End Base");
 
         First_Num = new JTextField("First num");
-
+        End_Base = new JTextField("Ending Base");
         text = new JLabel("+");
-
         Second_Num = new JTextField("Second num");
-
         First_Base = new JTextField("First Base");
         Second_Base = new JTextField("Second  base");
         end = new JTextField("answer",15);
@@ -48,9 +53,12 @@ public class gui extends JFrame implements ActionListener {
         add(Second_Base, layout);
         layout.gridx = 1;
         layout.gridy = 3;
-        add(Cal,layout);
+        add(End_Base,layout);
         layout.gridx = 1;
         layout.gridy = 4;
+        add(Cal,layout);
+        layout.gridx = 1;
+        layout.gridy = 5;
         add(end,layout);
 
         Cal.addActionListener(this);
@@ -67,12 +75,9 @@ public class gui extends JFrame implements ActionListener {
 
             String convert1 = String.valueOf(Integer.parseInt(String.valueOf(First_Num.getText()), Integer.parseInt(First_Base.getText())));
             String convert2 = String.valueOf(Integer.parseInt(String.valueOf(Second_Num.getText()), Integer.parseInt(Second_Base.getText())));
-            String x = convert1 + convert2;
+            int x = Integer.parseInt(convert1) + Integer.parseInt(convert2);
 
-           // System.out.println(Integer.toString(Integer.parseInt(x), 2));
-            Integer.toString(Integer.parseInt(Integer.toString(Integer.parseInt(x),2)));
-
-
+            end.setText(Integer.toString(Integer.parseInt(String.valueOf(x)), Integer.parseInt(End_Base.getText())));
         }
     }
 }
