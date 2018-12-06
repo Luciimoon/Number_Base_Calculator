ae313f57a04a805554745b9d1ce0103c462fcd95
compsci <mitchell_wallace@ymail.com>
2018-12-06 10:47:13 -0700

    wokrin on it

--- src/Joptionpane/gui.java
+++ src/Joptionpane/gui.java
@@ -25,10 +25,10 @@ public class gui extends JFrame implements ActionListener {
         Second_Base_Label = new JLabel("Second Base");
         End_Base_Label = new JLabel("End Base");
 
-        First_Num = new JTextField("First num");
+        First_Num = new JTextField("First num",15);
         End_Base = new JTextField("Ending Base");
         text = new JLabel("+");
-        Second_Num = new JTextField("Second num");
+        Second_Num = new JTextField("Second num",15);
         First_Base = new JTextField("First Base");
         Second_Base = new JTextField("Second  base");
         end = new JTextField("answer",15);
@@ -36,6 +36,9 @@ public class gui extends JFrame implements ActionListener {
         end.setEditable(false);
 
 
+        layout.gridx = 0;
+        layout.gridy = 1;
+        add(First_Num_Label, layout);
         layout.gridx = 1;
         layout.gridy = 1;
         add(First_Num, layout);
@@ -44,13 +47,25 @@ public class gui extends JFrame implements ActionListener {
         add(text, layout);
         layout.gridx = 3;
         layout.gridy = 1;
+        add(Second_Num_Label, layout);
+        layout.gridx = 4;
+        layout.gridy = 1;
         add(Second_Num, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 2;
+        add(Second_Base_Label, layout);
         layout.gridx = 1;
         layout.gridy = 2;
         add(First_Base, layout);
-        layout.gridx = 3;
+        layout.gridx = 2;
+        layout.gridy = 2;
+        add(Second_Base_Label, layout);
+        layout.gridx = 4;
         layout.gridy = 2;
         add(Second_Base, layout);
+
+
         layout.gridx = 1;
         layout.gridy = 3;
         add(End_Base,layout);
